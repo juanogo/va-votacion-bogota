513f411c62a7a5e51aea765d7ac185edd763928c
victor <vgarzom@gmail.com>
2018-12-01 00:30:52 -0500

    Implements de graphic interface structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeLayoutComponent } from './home-layout/home-layout.component'
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home',
+  component: HomeLayoutComponent,
+  data: { title: "Home" }
+},
+{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeLayoutComponent } from './home-layout/home-layout.component';
+import { HeaderComponent } from './home-layout/header/header.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLayoutComponent,
+    HeaderComponent,
+    RadarChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
